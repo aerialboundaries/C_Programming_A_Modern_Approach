04aee2193a156e1cf09675380b1059169446a485
Masato Takatani <takatani.masato@gmail.com>
2025-10-16 04:50:57 -0600

    ch4-10

---
+++ ch04/ex4-10.c
@@ -0,0 +1,50 @@
+/* C Programming A Modern Approach ex4-10.c
+ *
+ * Q: Show the output produced by each of the following program fragments.
+ * Asuume that i and j are int variables.
+ *
+ * (a) i = 6;
+ *      j = i += i;
+ *      printf("%d %d:, i, j);
+ * (b) i = 5;
+ *    j = (i -= 2) + 1;
+ *     printf("%d %d", i, j);
+ * (c) i = 7;
+ *      j = 6 + (i = 2.5);
+ *      printf("d %d", i, j);
+ * (d) i = 2; j = 8;
+ *      j = (i = 6) + (j = 3);
+ *      printf("d %d", i, j);
+ *
+ *  My Answers:
+ *  (a) 12 12
+ *  (b) 5 6
+ *  (c) 2 8
+ *  (d) 6 9
+ *
+ * $ ./a.out
+12 12
+3 4
+2 8
+6 9
+ */
+
+#include <stdio.h>
+
+int main(void) {
+  int i, j;
+
+  i = 6;
+  j = i += i;
+  printf("%d %d\n", i, j);
+  i = 5;
+  j = (i -= 2) + 1;
+  printf("%d %d\n", i, j);
+  i = 7;
+  j = 6 + (i = 2.5);
+  printf("%d %d\n", i, j);
+  i = 2;
+  j = 8;
+  j = (i = 6) + (j = 3);
+  printf("%d %d\n", i, j);
+}
