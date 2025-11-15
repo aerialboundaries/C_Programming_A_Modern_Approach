90d2bba01304b22322bb264777d03330040beaeb
Masato Takatani <takatani.masato@gmail.com>
2025-11-14 18:33:54 -0600

    dynabook

---
+++ ch07/length2.c
@@ -0,0 +1,19 @@
+/* C Programming A Modern Approach length2.c
+ * 2025-11-14
+ */
+
+/* Determines the length of a message */
+
+#include <stdio.h>
+
+int main(void) {
+  int len = 0;
+
+  printf("Enter a message: ");
+  while (getchar() != '\n') {
+    len++;
+  }
+  printf("Your message was %d character(s) long.\n", len);
+
+  return 0;
+}
