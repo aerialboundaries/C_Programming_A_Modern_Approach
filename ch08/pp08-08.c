4fd0923b74f6cb44e7a21375a6184815eff15d21
Masato Takatani <takatani.masato@gmail.com>
2025-11-28 19:08:34 -0600

    rz57

--- ch08/pp08-08.c
+++ ch08/pp08-08.c
@@ -14,47 +14,39 @@
 
 int main(void) {
 
-  int a[R][C] = {0}, row_total[R] = {0}, col_total[C] = {0}, h_score[R],
-      l_score[R], i, j;
-  double avg_score[C] = {0.0f}, g_avg_score[R] = {0.0f};
+  int a[R][C] = {0}, row_total[R] = {0}, col_total[C] = {0}, h_score[C],
+      l_score[C], i, j;
 
   for (i = 0; i < R; i++) {
     printf("Enter row %d: ", i + 1);
     for (j = 0; j < C; j++) {
       scanf(" %d", &a[i][j]);
+      row_total[i] += a[i][j];
+      col_total[j] += a[i][j];
+      if (i == 0) {
+        h_score[j] = a[i][j];
+        l_score[j] = a[i][j];
+      } else {
+        if (h_score[j] < a[i][j])
+          h_score[j] = a[i][j];
+        if (l_score[j] > a[i][j])
+          l_score[j] = a[i][j];
+      }
     }
   }
 
-  //   for (i = 0; i < 5; i++) {
-  //     for (j = 0; j < 5; j++) {
-  //       printf("%d ", a[i][j]);
-  //     }
-  //     printf("\n");
-  //   }
-
+  printf("\nBy Student\n");
   for (i = 0; i < R; i++) {
-    for (j = 0; j < C; j++) {
-      row_total[i] += a[i][j];
-    }
-    printf("Student %d:\n", i + 1);
+    printf("Student %d: ", i + 1);
     printf("Total Score: %d Average score %4.2f\n", row_total[i],
            (double)row_total[i] / C);
   }
 
-  for (i = 0; i < R; i++) {
-    for (j = 0; j < C; j++) {
-      col_total[j] += a[i][j];
-      if (a[i][j] > h_score[i])
-        h_score[j] = a[i][j];
-      if (a[i][j] < l_score[j])
-        l_score[j] = a[i][j];
-    }
-    g_avg_score[j] = (double)col_total[j] / R;
-  }
+  printf("\nBy Grade\n");
 
   for (j = 0; j < C; j++) {
-    printf("G %d", j);
-    printf("\n");
+    printf("Grade %d: High %4d Low %4d Avg %4.2f\n", j + 1, h_score[j],
+           l_score[j], (double)col_total[j] / R);
   }
 
   return 0;
