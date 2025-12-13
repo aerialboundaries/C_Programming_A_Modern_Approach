cc142243ad6c368272a5891461d9fd1c4db554aa
Masato Takatani <takatani.masato@gmail.com>
2025-12-12 23:10:29 -0600

    pun2.c

---
+++ ch09/pun2.c
@@ -0,0 +1,15 @@
+/* C Programming A Modern Approach
+ * pun2.c
+ * 2025-12-12
+ */
+
+/* Prints a bad pun */
+
+#include <stdio.h>
+
+void print_pun(void) { printf("To C, or not to C: that is the question.\n"); }
+
+int main(void) {
+  print_pun();
+  return 0;
+}
