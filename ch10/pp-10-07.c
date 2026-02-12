48f0b30c6e5eac026362d8a9f4e87426422b724c
Masato Takatani <takatani.masato@gmail.com>
2026-02-11 22:54:12 -0600

    rz57

--- ch10/pp-10-07.c
+++ ch10/pp-10-07.c
@@ -37,3 +37,52 @@
  * the digits array, each on a single line, producing output such as that
  * shown in the example.
  */
+
+#include <stdio.h>
+
+#define MAX_DIGITS 10
+#define IS_DIGIT(ch) (ch >= '0' && ch <= '9')
+
+/* Proto Type */
+void clear_digits_array(void);
+void process_digit(int digit, int position);
+void print_digits_array(void);
+
+/* external variables */
+const int seguments[10][7] = {{1, 1, 1, 1, 1, 1, 0}, {0, 1, 1, 0, 0, 0, 0},
+                              {1, 1, 0, 1, 1, 0, 1}, {1, 1, 1, 1, 0, 0, 1},
+                              {0, 1, 1, 0, 0, 1, 1}, {1, 0, 1, 1, 0, 1, 1},
+                              {1, 0, 1, 1, 1, 1, 1}, {1, 1, 1, 0, 0, 0, 0},
+                              {1, 1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 0, 1, 1}};
+
+char digits[4][MAX_DIGITS * 4];
+
+int main(void) {
+  int digit_count = 0, digit;
+  while ((digit = getchar()) && digit_count < MAX_DIGITS) {
+    if (IS_DIGIT(digit)) {
+      digit -= '0';
+      process_digit(digit, digit_count);
+      digit_count++;
+    }
+  }
+  return 0;
+}
+
+void clear_digits_array(void) {
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < MAX_DIGITS * 4; j++) {
+      digits[i][j] = ' ';
+    }
+  }
+}
+
+void process_digit(int digit, int position) {
+  int i = 0;
+  switch (digit) {
+  case 0:
+    for (i = 0; i < 3; i++) {
+      digits[position][i] =
+    }
+  }
+}
