03a4c14998f8f9348cd7bf70ff4a0a53d8f23540
Masato Takatani <takatani.masato@gmail.com>
2026-02-13 11:28:07 -0600

    dyna

---
+++ ch11/ex-11-01.c
@@ -0,0 +1,15 @@
+/**************************************************************************
+ * C Programming A Modern Approach                                         *
+ * ex-11-01.c                                                              *
+ * 2026-02-12                                                              *
+ *                                                                         *
+ * If i is a variable and p points to i, which of the following expressions*
+ * are aliases for i?                                                      *
+ *                                                                         *
+ * (a) *p    (c) *&p   (e) *i    (g) *&i                                   *
+ * (b) &p    (d) &*p   (f) &i    (h) &*i                                   *
+ *                                                                         *
+ * My Answer : (a), (g)                                                    *
+ *                                                                         *
+ *                                                                         *
+ **************************************************************************/
