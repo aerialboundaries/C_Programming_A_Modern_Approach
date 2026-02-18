f48df64d1393754eec5891294b1856f807c3a43e
Masato Takatani <takatani.masato@gmail.com>
2026-02-18 10:24:47 -0600

    ex-12-17.c complete

--- ch12/ex-12-17.c
+++ ch12/ex-12-17.c
@@ -32,9 +32,11 @@ int main(void) {
 }
 
 int sum_two_dimensional_array(const int a[][LEN], int n) {
-  int i, j, sum = 0;
-  for (i = 0; i < n; i++)
-    for (j = 0; j < LEN; j++)
-      sum += a[i][j];
+  int sum = 0;
+  const int *p;
+  for (p = a[0]; p < (a[0] + n * LEN); p++)
+
+    sum += *p;
+
   return sum;
 }
