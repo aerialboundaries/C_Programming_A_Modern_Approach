a4b290d2d01581c1916e5e350765892914bb11fe
Masato Takatani <takatani.masato@gmail.com>
2026-03-03 02:33:26 -0600

    rz

---
+++ ch13/ex-13-11.c
@@ -0,0 +1,17 @@
+/* C Programming A Modern Approach
+ * ex-13-11.c
+ * 2026-03-02
+ *
+ * The Q&A section at the end of this chapter shows how the strcmp function
+ * might be written using array subscripting. Modify the function to use pointer
+ * arithmetic instead.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#define N 80
+
+int my_strcmp(char *s, char *t);
+
+int main(void) { char s[80] }
