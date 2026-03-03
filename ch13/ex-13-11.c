732d0af83f179e19f6b42e36dac4ee0ff24f8064
Masato Takatani <takatani.masato@gmail.com>
2026-03-03 17:41:20 -0600

    ex-13-11-orig

--- ch13/ex-13-11.c
+++ ch13/ex-13-11.c
@@ -14,4 +14,22 @@
 
 int my_strcmp(char *s, char *t);
 
-int main(void) { char s[80] }
+int main(void)
+{
+    char s[80] = { "Thbs is a pen." };
+    char t[80] = { "That is a pen." };
+
+    printf("my_strcpm: %d\n", my_strcmp(s, t));
+
+    return 0;
+}
+
+int my_strcmp(char *s, char *t)
+{
+    int i;
+
+    for (i = 0; s[i] == t[i]; i++)
+        if (s[i] == '\0')
+            return 0;
+    return s[i] - t[i];
+}
