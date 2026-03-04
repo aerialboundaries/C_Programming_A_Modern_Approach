a352d37ba1e02fb34b0830bdac306cf4782be9c9
Masato Takatani <takatani.masato@gmail.com>
2026-03-03 22:14:58 -0600

    gemini

---
+++ ch13/ex-13-12-gemini.c
@@ -0,0 +1,54 @@
+/* C Programming A Modern Approach
+ * ex-13-12.c
+ * 2026-03-03
+ *
+ * Write the following function:
+ * void get_extension(const char *file_name, char *extension);
+ *
+ * file_name points to a string containing a file name. The function should
+ * store the extension on the file name in the string pointed to by extension.
+ * For example, if the file name is "memo.txt", the function will store "txt"
+ * in the string pointed to by extension. If the file name doesn't have an
+ * extension, the function should store an empty string (a single null
+ * character) in the string pointed to by extension. Keep the function as simple
+ * as possible by having it use the strlen and strcpy functions.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+void get_extension(const char *file_name, char *extension);
+
+int main(void) {
+  char file_name1[] = {"memo.txt"};
+  char file_name2[] = {"memo2"};
+  char file_name3[] = {"archive.tar.gz"};
+  char extension[20];
+
+  get_extension(file_name1, extension);
+  printf("file: %s -> extension: \"%s\"\n", file_name1, extension);
+
+  get_extension(file_name2, extension);
+  printf("file: %s -> extension: \"%s\"\n", file_name2, extension);
+
+  get_extension(file_name3, extension);
+  printf("file: %s -> extension: \"%s\"\n", file_name3, extension);
+
+  return 0;
+}
+
+void get_extension(const char *file_name, char *extension) {
+  int i;
+  int len = (int)strlen(file_name);
+
+  /* search '.' from the end */
+  for (i = len - 1; i >= 0; i--) {
+    if (file_name[i] == '.') {
+      /* if found copy the next string */
+      strcpy(extension, &file_name[i + 1]);
+      return;
+    }
+  }
+  /* if not found, set null */
+  strcpy(extension, "");
+}
