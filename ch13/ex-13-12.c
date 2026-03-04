0916502e4347301d3f81cf09626ce9b58a09d9f4
Masato Takatani <takatani.masato@gmail.com>
2026-03-03 19:30:23 -0600

    dyna

---
+++ ch13/ex-13-12.c
@@ -0,0 +1,47 @@
+/* C Programming A Modern Approach
+ * ex-13-12.c
+ * 2026-03-03
+ *
+ * Write the following function:
+ * void get_extension(const char *file_name, char *extension);
+ *
+ * file_name points to a string containing a file name. The function should
+ * store the extension on the file name in the string pointed to by extension.
+ * For example, if the file name is "memo.txt", the function will store "txt"
+ * in the string pointed to by extension. If the file name doesn't have an
+ * extension, the function should store an empty string (a single null
+ * character) in the string pointed to by extension. Keep the function as simple
+ * as possible by having it use the strlen and strcpy functions.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+void get_extension(const char *file_name, char *extension);
+
+int main(void)
+{
+    char file_name1[80] = { "memo.txt" };
+    char file_name2[80] = { "memo2" };
+    char extension[4];
+
+    get_extension(file_name1, extension);
+    printf("file: %s's extension: %s\n", file_name1, extension);
+
+    get_extension(file_name2, extension);
+    printf("file: %s's extension: %s\n", file_name2, extension);
+
+    return 0;
+}
+
+void get_extension(const char *file_name, char *extension)
+{
+    int i;
+    i = strlen(file_name);
+
+    while (*file_name != '\0') {
+        if (*file_name++ == '.')
+            break;
+        file_name++;
+    }
+}
