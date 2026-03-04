e88f8e2a9155d04748a892efe0c4349aa1a22f50
Masato Takatani <takatani.masato@gmail.com>
2026-03-03 21:50:19 -0600

    ex-13-12.c masato

--- ch13/ex-13-12.c
+++ ch13/ex-13-12.c
@@ -14,34 +14,31 @@
  * as possible by having it use the strlen and strcpy functions.
  */
 
+#include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 
 void get_extension(const char *file_name, char *extension);
 
-int main(void)
-{
-    char file_name1[80] = { "memo.txt" };
-    char file_name2[80] = { "memo2" };
-    char extension[4];
+int main(void) {
+  char file_name1[80] = {"memo.txt"};
+  char file_name2[80] = {"memo2"};
+  char extension[4];
 
-    get_extension(file_name1, extension);
-    printf("file: %s's extension: %s\n", file_name1, extension);
+  get_extension(file_name1, extension);
+  printf("file: %s's extension: %s\n", file_name1, extension);
 
-    get_extension(file_name2, extension);
-    printf("file: %s's extension: %s\n", file_name2, extension);
+  get_extension(file_name2, extension);
+  printf("file: %s's extension: %s\n", file_name2, extension);
 
-    return 0;
+  return 0;
 }
 
-void get_extension(const char *file_name, char *extension)
-{
-    int i;
-    i = strlen(file_name);
+void get_extension(const char *file_name, char *extension) {
+  size_t n;
+  strcpy(extension, "\0");
 
-    while (*file_name != '\0') {
-        if (*file_name++ == '.')
-            break;
-        file_name++;
-    }
+  for (n = 0; n < strlen(file_name); n++)
+    if (file_name[n] == '.')
+      strcpy(extension, &file_name[n + 1]);
 }
