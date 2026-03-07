b142529d6a7860fb1b400490550039b8eb55e694
Masato Takatani <takatani.masato@gmail.com>
2026-03-06 22:39:48 -0600

    pp-13-05.c original

---
+++ ch13/pp-13-05.c
@@ -0,0 +1,13 @@
+/* C Programming A Modern Approach
+ * pp-13-05.c
+ * 2026-03-06
+ *
+ * Write a propgram named sum.c that adds up its command-line arguments,
+ * which are assumed to be integers. Running the progaram by typin
+ * sum 8 24 62
+ * shoudl produce the following output;
+ * Total: 94
+ *
+ * Hint Use the atoi function to convert each comman-line argument fron
+ * storing to inter form.
+ */
