a4c6ee5b38bdeda0a721288819b76a689a0497ac
Masato Takatani <takatani.masato@gmail.com>
2026-03-06 18:30:32 -0600

    pp-13-02-b

--- ch13/pp-13-02.c
+++ ch13/pp-13-02.c
@@ -21,63 +21,63 @@
 #include <string.h>
 
 #define MAX_REMIND 50 /* maximum number of reminders */
-#define MSG_LEN 60 /* max length of reminder message */
+#define MSG_LEN 60    /* max length of reminder message */
 
 int read_line(char str[], int n);
 
-int main(void)
-{
-    char reminders[MAX_REMIND][MSG_LEN + 9];
-    char day_time_str[9], msg_str[MSG_LEN + 1];
-    int day, hour, minute, i, j, num_remind = 0;
+int main(void) {
+  char reminders[MAX_REMIND][MSG_LEN + 9];
+  char day_time_str[9], msg_str[MSG_LEN + 1];
+  int day, hour, minute, i, j, num_remind = 0;
 
-    for (;;) {
-        if (num_remind == MAX_REMIND) {
-            printf("-- No space left --\n");
-            break;
-        }
-
-        printf("Enter day time (ex: 24 13:15 and reminder: ");
-        scanf("%2d %2d:%2d", &day, &hour, &minute);
-        if (day == 0)
-            break;
-
-        if (day < 0 || day > 31 || hour < 0 || hour > 24 || minute < 0
-            || minute > 59) {
-            printf("invalid input\n");
-            while (getchar() != '\n')
-                ; // do nothing. clear buffer.
-            continue;
-        }
-
-        sprintf(day_time_str, "%2d %2d:%2d", day, hour, minute);
-        read_line(msg_str, MSG_LEN);
-
-        for (i = 0; i < num_remind; i++)
-            if (strcmp(day_time_str, reminders[i]) < 0)
-                break;
-        for (j = num_remind; j > i; j--)
-            strcpy(reminders[j], reminders[j - 1]);
-
-        strcpy(reminders[i], day_time_str);
-        strcat(reminders[i], msg_str);
-
-        num_remind++;
+  for (;;) {
+    if (num_remind == MAX_REMIND) {
+      printf("-- No space left --\n");
+      break;
     }
-    printf("\nDay Time Reminder\n");
-    for (i = 0; i < num_remind; i++)
-        printf(" %s\n", reminders[i]);
 
-    return 0;
+    printf("Enter day time (ex: 24 13:15 and reminder: ");
+    scanf("%2d", &day);
+    if (day == 0)
+      break;
+
+    scanf("%2d:%2d", &hour, &minute);
+
+    if (day < 0 || day > 31 || hour < 0 || hour > 24 || minute < 0 ||
+        minute > 59) {
+      printf("invalid input\n");
+      while (getchar() != '\n')
+        ; // do nothing. clear buffer.
+      continue;
+    }
+
+    sprintf(day_time_str, "%02d %02d:%02d", day, hour, minute);
+    read_line(msg_str, MSG_LEN);
+
+    for (i = 0; i < num_remind; i++)
+      if (strcmp(day_time_str, reminders[i]) < 0)
+        break;
+    for (j = num_remind; j > i; j--)
+      strcpy(reminders[j], reminders[j - 1]);
+
+    strcpy(reminders[i], day_time_str);
+    strcat(reminders[i], msg_str);
+
+    num_remind++;
+  }
+  printf("\nDay Time Reminder\n");
+  for (i = 0; i < num_remind; i++)
+    printf(" %s\n", reminders[i]);
+
+  return 0;
 }
 
-int read_line(char str[], int n)
-{
-    int ch, i = 0;
+int read_line(char str[], int n) {
+  int ch, i = 0;
 
-    while ((ch = getchar()) != '\n')
-        if (i < n)
-            str[i++] = ch;
-    str[i] = '\0';
-    return i;
+  while ((ch = getchar()) != '\n')
+    if (i < n)
+      str[i++] = ch;
+  str[i] = '\0';
+  return i;
 }
