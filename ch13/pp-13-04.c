0eab50d8470e3aa92314419f2ac85e306941661f
Masato Takatani <takatani.masato@gmail.com>
2026-03-06 20:46:25 -0600

    pp-13-04.c complete

--- ch13/pp-13-04.c
+++ ch13/pp-13-04.c
@@ -2,4 +2,21 @@
  * pp-13-04.c
  * 2026-03-06
  *
+ * Write a program named reverse.c that echos its command-line arguments
+ * in reverse order. Running the program by typing
+ *
+ * reverse void and null
+ *
+ * should produce the following output:
+ *
+ * null and void
  */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  int i;
+  for (i = argc - 1; i > 0; i--)
+    printf("%s ", argv[i]);
+  printf("\n");
+}
