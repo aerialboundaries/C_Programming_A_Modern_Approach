a4b5dd80b8c019a179e171c1cf6740e4eba22cc6
Masato Takatani <takatani.masato@gmail.com>
2026-03-08 21:52:50 -0600

    pp-13-06.c -KN

--- ch13/pp-13-05.c
+++ ch13/pp-13-05.c
@@ -11,3 +11,16 @@
  * Hint Use the atoi function to convert each comman-line argument fron
  * storing to inter form.
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  int sum = 0;
+  for (int i = 1; i < argc; i++)
+    sum += atoi(argv[i]);
+
+  printf("Total: %d\n", sum);
+
+  return 0;
+}
---
+++ ch13/pp-13-06.c
@@ -0,0 +1,51 @@
+/* C Programming A Modern Approach
+ * pp-13-06.c
+ * 2026-03-08
+ *
+ * Improve the planet.c program of Section 13.7 by having it igonre case
+ * when comparing command-line arguments with strings in the planets arrayl.
+ */
+
+/* Checks planet names */
+
+/* Checks planet names */
+
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+#define NUM_PLANETS 9
+
+int string_equal(const char *s, const char *t);
+
+int main(int argc, char *argv[]) {
+  char *planets[] = {"Mercury", "Venus",  "Earth",   "Mars", "Jupiter",
+                     "Saturn",  "Uranus", "Neptune", "Pluto"};
+
+  int i, j;
+  char *p, *q;
+
+  for (i = 1; i < argc; i++) {
+    for (j = 0; j < NUM_PLANETS; j++) {
+      if (string_equal(argv[i], planets[j])) {
+        printf("%s is planet %d\n", argv[i], j + 1);
+        break;
+      }
+    }
+
+    if (j == NUM_PLANETS)
+      printf("%s is not a planet\n", argv[i]);
+  }
+
+  return 0;
+}
+
+int string_equal(const char *s, const char *t) {
+  int i;
+
+  for (i = 0; toupper(s[i]) == toupper(t[i]); i++)
+    if (s[i] == '\0')
+      return 1;
+
+  return 0;
+}
