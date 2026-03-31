4327633718e153a418aa65092a9efdab0f32be24
Masato Takatani <takatani.masato@gmail.com>
2026-03-31 17:01:07 -0600

    ex-14-17.c

---
+++ ch16/ex-14-17.c
@@ -0,0 +1,21 @@
+/* C Programming A Modern Approach
+ * ex-16-17.c
+ * 2016-03-31
+ *
+ * Suppose that b and i are declared as follows:
+ * enum {FALSE, TRUE} b;
+ * int i;
+ *
+ * Which of the following statements are legal?
+ * Which ones are "safe" (alway yield a meaningful result):
+ *
+ * (a) b = FALSE;
+ * (b) b = i;
+ * (c) b++;
+ * (d) i = b;
+ * (e) i = 2 * b + 1;
+ *
+ * My Answer:
+ *
+ *
+ */
