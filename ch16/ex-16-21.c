55eaee3de235b2daad0ece24eaa3c7712a01a343
Masato Takatani <takatani.masato@gmail.com>
2026-04-01 15:49:07 -0600

    ex-16-21.c

---
+++ ch16/ex-16-21.c
@@ -0,0 +1,20 @@
+/* C Programming A Modern Approach ***************************************
+ * ex-16-21.c
+ * 2026-04-01
+ *
+ * What are the integer values of the enumeration constants in each of
+ * the follwoing declarations?
+ * (a) enum {NUL, SOH, STX, ETX};
+ *     My answer {0, 1, 2, 3}
+ * (b) enum {VT = 11, FF, CR};
+ *     My answer {11, 12, 13}
+ * (c) enum {SO = 14, SI, DLE, CAN = 24, EM};
+ *     My answer {14, 15, 16, 24, 25}
+ * (d) enum {ENQ = 45, ACK, BEL, LF = 37, ETB, ESC};
+ *     My answer {45, 46, 47, 37, 38, 39}
+ *
+ *************************************************************************/
+
+void exercise(void)
+{
+}
