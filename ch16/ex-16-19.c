858f013afea0af43c4b06d8d12f97f6259139666
Masato Takatani <takatani.masato@gmail.com>
2026-04-01 03:36:31 -0600

    rp3

---
+++ ch16/ex-16-19.c
@@ -0,0 +1,12 @@
+/* C Programming A Modern Approach
+ * ex-16-19.c
+ * 2026-03-31
+ *
+ * Declare a structure with the following members whose tag is pinball_machine:
+ * nama-a string of up to 40 characters
+ * year - an integer (representing the year of manuactreu)
+ * type - an enumeration with the values EM (electromechanical) and SS
+ * (solid stage)
+ * players - an integer (representing the maximmmum number of players)
+ *
+ */
