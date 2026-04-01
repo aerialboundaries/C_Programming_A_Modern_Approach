e272c1afc4353bb07aa68f2692449cf6b58145cc
Masato Takatani <takatani.masato@gmail.com>
2026-03-31 18:54:53 -0600

    ex-16-17.c

--- ch16/ex-16-17.c
+++ ch16/ex-16-17.c
@@ -1,6 +1,6 @@
 /* C Programming A Modern Approach
  * ex-16-17.c
- * 2016-03-31
+ * 2026-03-31
  *
  * Suppose that b and i are declared as follows:
  * enum {FALSE, TRUE} b;
@@ -15,7 +15,10 @@
  * (d) i = b;
  * (e) i = 2 * b + 1;
  *
- * My Answer:
- *
+ * My Answer:(KN)
+ * All the statements are legal, since C allows integers and enumeration
+ * values to be mixed without restriction. Only (a), (d), and (e) are safe.
+ * (b) is not meaningful if i has a value other than 0 or 1. (c) will not
+ * yield a meaningful result if b has the value 1.
  *
  */
