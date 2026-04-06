7ab074a50206b09cb4712456a1195d78e10a9c37
Masato Takatani <takatani.masato@gmail.com>
2026-04-05 20:14:34 -0600

    review ch03

---
+++ ch03/review_ch03/pp3-1.c
@@ -0,0 +1,19 @@
+/* C Programming A Modern Approach CH03 Programmming Promects 3.1 */
+// 2026-04-05
+
+/* Write a program that accepts a date from the user inthe form mm/dd/yyyy and
+ * then displays it in the form of yyyymmdd:
+ *
+ * Enter a date (mm/dd/yyyy): 2/17/2011
+ * You entered the date 20110217 */
+#include <stdio.h>
+
+int main(void) {
+  int m, d, y;
+  printf("Enter a date (mm/dd/yyyy): ");
+  scanf("%d/%d/%d", &m, &d, &y);
+
+  printf("You entered the date %4d%.2d%.2d\n", y, m, d);
+
+  return 0;
+}
