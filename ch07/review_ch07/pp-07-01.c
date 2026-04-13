11ad20be42f59037c96015084b05e684bf45f879
Masato Takatani <takatani.masato@gmail.com>
2026-04-12 23:14:12 -0600

    pp-07-02.c

---
+++ ch07/review_ch07/pp-07-01.c
@@ -0,0 +1,30 @@
+/* C Programming A Modern Approach pp7-01.c
+ * 2025-11-16 revisited 2026-04-12
+ *
+ * The square2.c program of Section 6.3 will fail (usually by printing
+ * strange answers) if i * i exceeds the maximum int value. Run the
+ * program and determie the smallest value of n that causes failure.
+ * Try changing the type of i to short and running the program again.
+ * (Don't forget to update the conversion specifications in the call
+ * of printf!) Then try long. From these experiments, what can you
+ * conclude about the number of bits used to store integer types on
+ * your machine? */
+
+/* C Programming A Modern Approach square2.c
+ * 2025-10-28
+ */
+
+#include <stdio.h>
+
+int main(void) {
+  int i, n;
+
+  printf("This program prints a table of squares.\n");
+  printf("Enter number of entries in table: ");
+  scanf("%d", &n);
+
+  for (i = 1; i <= n; i++)
+    printf("%10d%10d\n", i, i * i);
+
+  return 0;
+}
---
+++ ch07/review_ch07/pp-07-02.c
@@ -0,0 +1,44 @@
+/* C Programming A Modern Approach pp07-02.c
+ * 2025-11-17 revisited 2026-04-12
+ *
+ * Modify the square2.c program of Section 6.3 so that it pauses after
+ * every 24 squares and displays the following message:
+ *
+ * Press Enter to cntinue...
+ *
+ * After displaying the message, the program shuld use getchar to read
+ * a character. getchar won't allow the program to continue until the
+ * user presses the Enter key.
+ */
+
+/* C Programming A Modern Approach square2.c
+ * 2025-10-28
+ */
+
+#include <stdio.h>
+
+int main(void) {
+  int i, n;
+  char ch;
+
+  printf("This program prints a table of squares.\n");
+  printf("Enter number of entries in table: ");
+  scanf("%d", &n);
+  while ((ch = getchar()) != '\n' && ch != EOF)
+    ; // Null statement to clear the rest of input ('\n')
+
+  for (i = 1; i <= n; i++) {
+    if (i % 24) {
+      printf("%10d%10d\n", i, i * i);
+    } else {
+      printf("%10d%10d\n", i, i * i);
+      printf("Press Enter to continue...\n");
+      if ((ch = getchar()) == '\n' || ch == EOF)
+        continue;
+      else
+        return 0;
+    }
+  }
+
+  return 0;
+}
