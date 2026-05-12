c4c85dc2f26031e6988a7738bdc9350427846e64
Masato Takatani <takatani.masato@gmail.com>
2026-05-11 22:06:01 -0600

    ex-18-12.c

--- ch18/ex-18-12.c
+++ ch18/ex-18-12.c
@@ -7,5 +7,17 @@ ex-18-12.c
 
     int (*f(float (*)(long), char *)) (double);
 
+    My Answer:
+    f is a function with two arguments.  First argument is a pointer to a
+fuction whicha takes long int as arugument and returns float.  Second argument
+is char *, it returns the pointer to a fuction which takes double as an argument
+and return integer.
+
 (b) Give an example showing how f would be called.
+My Answer:
+float my_callback(long 1) {
+  return (float)l;
+}
+
+char *str = "Hello";
 */
