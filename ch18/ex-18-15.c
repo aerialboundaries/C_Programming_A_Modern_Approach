621d45f5d8f0f46cadb85dcbfafec1e2a491ce35
Masato Takatani <takatani.masato@gmail.com>
2026-05-11 23:59:12 -0600

    ex-18-15.c

---
+++ ch18/ex-18-15.c
@@ -0,0 +1,11 @@
+/* C Programming A Modern Approach
+ex-18-15.c
+2026-05-11
+
+Which property of a variable determines whether or not it has a default initial
+value? (a) Storage duration (b) Scope (c) Linkage (d) Type
+
+My answer:
+(a). Variables with static storage duration are initialized by zero by default.
+variables with automatic storage duration have no initial value.
+*/
