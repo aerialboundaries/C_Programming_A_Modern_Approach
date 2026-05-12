bef9cc1476f5c337786773db429e5d0a1e0fbbc7
Masato Takatani <takatani.masato@gmail.com>
2026-05-11 20:59:26 -0600

    ex-18-12 ongoing

---
+++ ch18/ex-18-12.c
@@ -0,0 +1,11 @@
+/* C Programming A Modern Approach
+ex-18-12.c
+2026-05-11
+
+(a) Write a complete description of the type of the function f, assuming that
+    it's declared as follows:
+
+    int (*f(float (*)(long), char *)) (double);
+
+(b) Give an example showing how f would be called.
+*/
