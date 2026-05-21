d124c126c57541a5a69b01861d8a4a4e2b221890
Masato Takatani <takatani.masato@gmail.com>
2026-05-21 12:06:13 -0600

    ex-19-01.c

--- ch19/ex-19-01.c
+++ ch19/ex-19-01.c
@@ -13,3 +13,42 @@ queue might include:
 Write an interface for a queue module in the form of a header file named
 queue.h.
 */
+
+/* queue.h */
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <stdbool.h>
+
+// data type to store in queue
+typedef int Item;
+
+// incomplete declaration of queue structure
+typedef struct queue_type *Queue;
+
+// function to create queue
+Queue create(void);
+
+// function to destroy and free memory
+void destroy(Queue q);
+
+// add item to the last of queue (Insert)
+void enqueue(Queue q, Item i);
+
+// Remove item from top of queue (Remove)
+// The oldest item is returned and deleted from queue
+Item dequeue(Queue q);
+
+// peek first item (Return first)
+// return the oldest item, but not delete
+Item peek_first(Queue q);
+
+// peek the last item (Return last)
+// returns the latest item, but not delete
+Item peek_last(Queue q);
+
+// test if the queue is empty (Test empty)
+// if empty returns true, if not false
+bool is_empty(Queue q);
+
+#endif
