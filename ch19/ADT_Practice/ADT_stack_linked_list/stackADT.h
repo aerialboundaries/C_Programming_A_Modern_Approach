06fcaf1650c201adedb4b013a1b658f915eee43c
Masato Takatani <takatani.masato@gmail.com>
2026-06-04 17:14:15 -0600

    dyna

---
+++ ch19/ADT_Practice/ADT_stack_linked_list/stackADT.h
@@ -0,0 +1,17 @@
+#ifndef STACKADT_H
+#define STACKADT_H
+
+#include <stdbool.h>
+
+typedef struct stack_type *Stack;
+typedef int Item;
+
+Stack create();
+void destroy(Stack s);
+bool is_empty(Stack s);
+bool is_full(Stack s);
+void make_empty(Stack s);
+void push(Stack s, Item i);
+Item pop(Stack s);
+
+#endif
