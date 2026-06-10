91cd00dcee7c08f609eac1cd9548eb312af76a81
Masato Takatani <takatani.masato@gmail.com>
2026-06-09 22:38:50 -0600

    ex-20-08.c ongoing

---
+++ ch20/ex-20-08.c
@@ -0,0 +1,17 @@
+// 2026-06-09
+
+/*
+Let f be the following function:
+unsigned int f(unsigned int i, int m, int n)
+{
+  return (i >> (m +1-n)) & ~(~0 << n);
+}
+(a) What is the value of ~ (~0 << n) ?
+(b) What does this function do?
+
+My Answer:
+(a)
+
+(b)
+
+*/
