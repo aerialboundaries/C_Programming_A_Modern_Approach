1fdf9a5ba776d67b1e3bb3694acde33e8c79b1d7
Masato Takatani <takatani.masato@gmail.com>
2026-06-10 18:07:49 -0600

    pp-20-01.c

--- ch20/pp-20-01.c
+++ ch20/pp-20-01.c
@@ -2,9 +2,39 @@
 
 /*
 Design a union that makes it possible to view a 32-bit value as either a float
-or the struc- ture described in Exercise 14. Write a program that stores 1 in
+or the structure described in Exercise 14. Write a program that stores 1 in
 the structure's sign field, 128 in the exponent field, and 0 in the fraction
 field, then prints the float value stored in the union. (The answer should
 be-2.0 if you've set up the bit-fields correctly.)
-
 */
+
+/*
+When stored according to the IEEE floating-point standard, a float value
+consists of a 1- bit sign (the leftmost-or most significant-bit), an 8-bit
+exponent, and a 23-bit fraction, in that order. Design a structure type that
+occupies 32 bits, with bit-field members corresponding to the sign, exponent,
+and fraction. Declare the bit-fields to have type unsigned int. Check the manual
+for your compiler to determine the order of the bit-fields.
+*/
+#include <stdio.h>
+
+union {
+  struct {
+    unsigned int fraction : 23;
+    unsigned int exponent : 8;
+    unsigned int sign : 1;
+  } f1;
+
+  float f;
+} flt;
+
+int main(void)
+{
+  flt.f1.sign = 1;
+  flt.f1.exponent = 128;
+  flt.f1.fraction = 0;
+
+  printf("%4.2f\n", flt.f);
+
+  return 0;
+}
