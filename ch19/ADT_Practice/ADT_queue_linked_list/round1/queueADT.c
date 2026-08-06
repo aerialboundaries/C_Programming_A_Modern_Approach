ec56f5f318caca319971f7f5a6daa5ae018cee16
Masato Takatani <takatani.masato@gmail.com>
2026-08-06 17:35:50 -0600

    gp.md

---
+++ ch19/ADT_Practice/ADT_queue_linked_list/round1/queueADT.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "queueADT.h"
+
+struct node {
+  Item data;
+  struct node *next;
+};
+
+struct queue_type {
+  struct node *head;
+  struct node *tail;
+  int size;
+};
+
+static void terminate(const char *message)
+{
+  printf("%s\n", message);
+  exit(EXIT_FAILURE);
+}
+
+Queue create(void)
+{
+  Queue q = malloc(sizeof(struct queue_type));
+  if (q == NULL)
+    terminate("Error in create: queue could not be created.");
+  q->head = q->tail = NULL;
+  q->size = 0;
+  return q;
+}
+
+void destroy(Queue q)
+{
+  make_empty(q);
+  free(q);
+}
+
+void make_empty(Queue q);
+
+bool is_empty(Queue q)
+{
+  return q->size == 0;
+}
+
+bool is_full(Queue q)
+{
+  return false;
+}
+
+void enqueue(Queue q, Item i)
+{
+  if (is_full(q))
+    terminate("Error in enqueue: queue is full.");
+  struct node *new_node = malloc(sizeof(struct node));
+  if (new_node == NULL)
+    terminate("Error in enqueue: queue is full.");
+  new_node->data = i;
+  new_node->next = NULL;
+
+  if (is_empty(q)) {
+    q->head = new_node;
+  } else {
+    q->tail->next = new_node;
+  }
+  q->tail = new_node;
+  q->size++;
+}
+
+Item dequeue(Queue q)
+{
+  if (is_empty(q))
+    terminate("Error in dequeue: queue is empty.");
+  struct node *old_node = q->head;
+  Item i = old_node->data;
+
+  q->head = old_node->next;
+  if (q->head == NULL)
+    q->tail = NULL;
+
+  free(old_node);
+  q->size--;
+  return i;
+}
+
+Item peek_head(Queue q)
+{
+  if (is_empty(q))
+    terminate("Error in peek_head: queue is empty.");
+  return q->head->data;
+}
+
+Item peek_tail(Queue q)
+{
+  if (is_empty(q))
+    terminate("Error in peek_head: queue is empty.");
+  return q->tail->data;
+}
