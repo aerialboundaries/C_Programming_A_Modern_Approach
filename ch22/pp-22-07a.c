a06fcc05bed2a182c8de1484536e53e80f92204e
Masato Takatani <takatani.masato@gmail.com>
2026-08-06 18:32:36 -0600

    rz57

--- ch22/pp-22-07a.c
+++ ch22/pp-22-07a.c
@@ -5,45 +5,47 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   if (argc != 2) {
-    perror(argv[1]);
+    fprintf(stderr, "Usage %s <filename>\n", argv[0]);
     exit(EXIT_FAILURE);
   }
 
   FILE *fp;
   if ((fp = fopen(argv[1], "rb")) == NULL) {
-    fprintf(stderr, "Cannot open %s\n", argv[1]);
+    perror(argv[1]);
     exit(EXIT_FAILURE);
   }
 
-  char rle_filename[strlen(argv[1]) + 4];
+  char rle_filename[strlen(argv[1]) + 5];
   snprintf(rle_filename, sizeof(rle_filename), "%s.rle", argv[1]);
 
   FILE *dest_fp;
   if ((dest_fp = fopen(rle_filename, "wb")) == NULL) {
-    fprintf(stderr, "Cannot open %s\n", rle_filename);
-    ;
+    perror(rle_filename);
     fclose(fp);
     exit(EXIT_FAILURE);
   }
 
-  int base_ch, ch, seq = 1;
-  base_ch = getc(fp);
+  int base_ch = getc(fp);
+  if (base_ch != EOF) {
+    int ch, seq = 1;
 
-  while ((ch = getc(fp)) != EOF || ferror(fp)) {
-    if (ch == base_ch) {
-      seq++;
-    } else {
-      putc(seq, dest_fp);
-      putc(base_ch, dest_fp);
-      seq = 1;
-      base_ch = ch;
+    while ((ch = getc(fp)) != EOF) {
+      // if seq exceeds 255 writeout. for safety
+      if (ch == base_ch && seq < 255) {
+        seq++;
+      } else {
+        putc(seq, dest_fp);
+        putc(base_ch, dest_fp);
+        seq = 1;
+        base_ch = ch;
+      }
     }
-    if (ch == EOF) {
-      putc(seq, dest_fp);
-      putc(ch, dest_fp);
-    }
+    // write out the last buffer
+    putc(seq, dest_fp);
+    putc(base_ch, dest_fp);
   }
 
   fclose(fp);
